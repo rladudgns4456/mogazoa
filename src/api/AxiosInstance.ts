e5737b447cb157ec981eee99001384b8faba51d1
YooJeYoung <jdngh@naver.com>
2025-11-14 15:34:25 +0900

    [추가] 로그인 페이지, 로그인시 쿠키저장

--- src/api/AxiosInstance.ts
+++ src/api/AxiosInstance.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE,
@@ -6,5 +7,39 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+// 요청 인터셉터: 모든 요청에 액세스 토큰 추가
+axiosInstance.interceptors.request.use(
+  config => {
+    const accessToken = Cookies.get("accessToken");
+
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+// 응답 인터셉터: 401 에러 처리 등
+axiosInstance.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    if (error.response?.status === 401) {
+      // 토큰이 만료되었거나 유효하지 않은 경우
+      Cookies.remove("accessToken");
+      // 필요시 로그인 페이지로 리다이렉트
+      if (typeof window !== "undefined") {
+        window.location.href = "/login";
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
 export default axiosInstance;
