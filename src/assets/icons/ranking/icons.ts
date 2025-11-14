6aa84501e8bc73715ebf6a6ca3cc75a78c4a484b
DESKTOP-0JE7RRL\82105 <gowoonsorr@gmail.com>
2025-11-15 02:09:35 +0900

    [추가] 리뷰랭킹 스토리북 추가 + 랭킹뱃지 이미지 인식되게 수정

--- src/assets/icons/ranking/icons.ts
+++ src/assets/icons/ranking/icons.ts
@@ -1,9 +1,9 @@
-import ic_rank_1 from "./ic_rank_1.svg";
-import ic_rank_2 from "./ic_rank_2.svg";
-import ic_rank_3 from "./ic_rank_3.svg";
-import ic_rank_m1 from "./ic_rank_m1.svg";
-import ic_rank_m2 from "./ic_rank_m2.svg";
-import ic_rank_m3 from "./ic_rank_m3.svg";
+import ic_rank_1 from "./ic_rank_1.svg?react";
+import ic_rank_2 from "./ic_rank_2.svg?react";
+import ic_rank_3 from "./ic_rank_3.svg?react";
+import ic_rank_m1 from "./ic_rank_m1.svg?react";
+import ic_rank_m2 from "./ic_rank_m2.svg?react";
+import ic_rank_m3 from "./ic_rank_m3.svg?react";
 
 export const rankingIcons = {
   ic_rank_1,
