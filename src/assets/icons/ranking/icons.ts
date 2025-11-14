81d07efd531ac18bca7994bb126e51ac90a62461
DESKTOP-0JE7RRL\82105 <gowoonsorr@gmail.com>
2025-11-15 01:43:53 +0900

    [수정] 리뷰랭킹뱃지 컴포넌트 수정 및 모바일 뱃지아이콘 추가

--- src/assets/icons/ranking/icons.ts
+++ src/assets/icons/ranking/icons.ts
@@ -1,13 +1,17 @@
 import ic_rank_1 from "./ic_rank_1.svg";
 import ic_rank_2 from "./ic_rank_2.svg";
 import ic_rank_3 from "./ic_rank_3.svg";
-import ic_rank_4 from "./ic_rank_4.svg";
+import ic_rank_m1 from "./ic_rank_m1.svg";
+import ic_rank_m2 from "./ic_rank_m2.svg";
+import ic_rank_m3 from "./ic_rank_m3.svg";
 
 export const rankingIcons = {
   ic_rank_1,
   ic_rank_2,
   ic_rank_3,
-  ic_rank_4,
+  ic_rank_m1,
+  ic_rank_m2,
+  ic_rank_m3,
 } as const;
 
 export type RankingIconKey = keyof typeof rankingIcons;
