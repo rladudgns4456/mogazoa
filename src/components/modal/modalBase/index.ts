7b7e40235cf9a4b4634ac03f10ec83c222bbaccf
kim-hyunhwa <pinpa@naver.net>
2025-11-19 19:40:47 +0900

    [추가] 모달 Portal 생성, [기타] context에서 모달 관리 되도록 수정

---
+++ src/components/modal/modalBase/index.ts
@@ -0,0 +1,3 @@
+export { default as ModalWrap } from "@/components/modal/modalBase/modalWrap";
+export { default as ModalContainer } from "@/components/modal/modalBase/ModalContainer";
+export { ModalProvider, useModal } from "@/components/modal/modalBase/modalProvider";
