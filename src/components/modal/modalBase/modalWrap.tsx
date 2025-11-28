1ca92f7523355692d5ac69ea2e7260346fdf0dfb
Younghoon <rladudgns4456@naver.com>
2025-11-29 02:51:51 +0900

    [수정] 모든 파일 에러 검수

--- src/components/modal/modalBase/modalWrap.tsx
+++ src/components/modal/modalBase/modalWrap.tsx
@@ -9,7 +9,7 @@ const ModalWrap = () => {
       {modalState.isOpen &&
         createPortal(
           <div
-            className="black-op30 fixed inset-0 z-[100] min-h-full p-17 overflow-y-auto overscroll-none"
+            className="black-op30 fixed inset-0 z-[100] min-h-full overflow-y-auto overscroll-none p-17"
             onClick={() => {
               closeModal();
             }}
